b73a9eb3cda83f67fb0c121a31ba9c7faa91eb03
“Dorce <dorce.raphael@gmail.com>
2021-09-13 13:41:01 +0200

    ex2 added localstorage

--- jour2/exercice2/app.js
+++ jour2/exercice2/app.js
@@ -1,20 +1,32 @@
 const letters = "^[a-zA-Z_-]*$";
 let persons = [];
+let personsPicked = [];
+
+if (JSON.parse(localStorage.getItem("persons", persons))) {
+  persons = JSON.parse(localStorage.getItem("persons", persons));
+  personsPicked = JSON.parse(
+    localStorage.getItem("personsPicked", personsPicked)
+  );
+}
+console.log(persons);
+console.log(personsPicked);
+
 oneResult = document.querySelector(".one");
 document.querySelector("#add-btn").addEventListener("click", function () {
   let addInput = document.querySelector("#add-input").value;
   if (addInput != "" && addInput.match(letters)) {
     persons.push(addInput);
-    console.log(persons);
+    localStorage.setItem("persons", JSON.stringify(persons));
   } else {
     oneResult.innerHTML = `<p>BRUH</p>`;
   }
 });
 
 document.querySelector("#pick-btn").addEventListener("click", function () {
-  const random = Math.floor(Math.random() * persons.length);
+  let random = Math.floor(Math.random() * persons.length);
+  personsPicked.push(persons[random]);
   oneResult.innerHTML = `<p>${persons[random]}</p>`;
   persons.splice(random, 1);
-  console.log();
-  console.log(persons);
+  localStorage.setItem("persons", JSON.stringify(persons));
+  localStorage.setItem("personsPicked", JSON.stringify(personsPicked));
 });
