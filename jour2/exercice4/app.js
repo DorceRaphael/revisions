ed4bfe733906e0cc5ff745ae902d193e52fe269b
“Dorce <dorce.raphael@gmail.com>
2021-09-14 10:50:23 +0200

    ex4 ajax done

--- jour2/exercice4/app.js
+++ jour2/exercice4/app.js
@@ -1,22 +1,41 @@
 const letters = "^[a-zA-Z_-]*$";
 oneResult = document.querySelector(".one");
-var data = "";
-var dataFiltered = "";
+let data = "";
+let dataFiltered = "";
 let countryNbr = document.querySelector("#country-nbr");
+
+function getSelectedCheckboxValues(name) {
+  const checkboxes = document.querySelectorAll(`input[name="${name}"]:checked`);
+  let values = [];
+  checkboxes.forEach((checkbox) => {
+    values.push(checkbox.value);
+  });
+  return values;
+}
 
 document.querySelector("#check-btn").addEventListener("click", function (e) {
   e.preventDefault();
-  value = countryNbr.options[countryNbr.selectedIndex].value;
-  loadData(value, "Africa");
+  oneResult.innerHTML = "";
+  contryNbrValue = countryNbr.options[countryNbr.selectedIndex].value;
+  for (let i = 0; i < getSelectedCheckboxValues("continent").length; i++) {
+    loadData(contryNbrValue, getSelectedCheckboxValues("continent")[i]);
+  }
 });
 
-function loadData(slice, country) {
+function loadData(sliceParam, countryParam) {
   const xhttp = new XMLHttpRequest();
   xhttp.onload = function () {
     data = JSON.parse(this.responseText);
-    data = data.filter((element) => element.continent == country);
-    data = data.slice(0, slice);
+    data = data.filter((element) => element.continent == countryParam);
+    data = data.slice(0, sliceParam);
     console.log(data);
+    console.log(data.length);
+    for (let i = 0; i < data.length; i++) {
+      let dataItem = document.createElement("div");
+      dataItem.innerHTML = `<p class="continent">${data[i].continent}</p>
+      <p class="country">${data[i].country}</p>`;
+      oneResult.appendChild(dataItem);
+    }
   };
   xhttp.open("GET", "data.json", true);
   xhttp.send();
